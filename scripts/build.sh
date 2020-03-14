bf090328fb1c6a88461b1fcd3e3ba84070dbf465
stdmatt <stdmatt@pixelwizards.io>
2020-03-14 21:31:30 +0300

    Add the version file

--- scripts/build.sh
+++ scripts/build.sh
@@ -26,14 +26,14 @@ source /usr/local/src/stdmatt/shellscript_utils/main.sh
 ##----------------------------------------------------------------------------##
 ## Variables                                                                  ##
 ##----------------------------------------------------------------------------##
-PROJECT_NAME="color grid";
-PROJECT_PACKAGE_NAME="color_grid";
+PROJECT_NAME="nuclear rain";
+PROJECT_PACKAGE_NAME="nuclear_rain";
 
 SCRIPT_DIR="$(pw_get_script_dir)";
 ROOT_DIR="$(pw_abspath "${SCRIPT_DIR}/..")";
 BUILD_DIR="${ROOT_DIR}/build";
 DIST_DIR="${ROOT_DIR}/dist";
-TO_COPY="libs/ src/ res/ css/ img/ index.html";
+TO_COPY="libs/ src/ res/ css/ index.html";
 
 
 ##----------------------------------------------------------------------------##
